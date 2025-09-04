299a8fb74028cb83b2bbc04787078777fbc0f025
pasoge6131 <47226786-pasoge6131@users.noreply.replit.com>
2025-09-04 12:15:25 +0000

    Restored to '3863ce598cd21de0cbe56cc75a8840949338db6b'
    Replit-Restored-To: 3863ce598cd21de0cbe56cc75a8840949338db6b

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Footer from "@/components/footer";
 import Home from "@/pages/home.tsx";
 import Properties from "@/pages/properties.tsx";
 import PropertyDetail from "@/pages/property-detail.tsx";
@@ -27,13 +26,8 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <div className="min-h-screen flex flex-col">
-          <div className="flex-1">
-            <Toaster />
-            <Router />
-          </div>
-          <Footer />
-        </div>
+        <Toaster />
+        <Router />
       </TooltipProvider>
     </QueryClientProvider>
   );
